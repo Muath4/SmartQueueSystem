f8a773a3423d224af8ace388a86626c1f2879475
Muath Alshathri <abogbl4@gmail.com>
2021-04-07 17:40:06 +0300

    ...

--- app/src/main/java/com/example/myapplication/objects/statistic.java
+++ app/src/main/java/com/example/myapplication/objects/statistic.java
@@ -1,8 +1,15 @@
 package com.example.myapplication.objects;
 
+import java.util.Date;
+
 public class statistic {
     private int timesTicketCanceled;
     private int timesTicketCompleted;
     private int timesCustomerOutRangeAfterBookTicket;
+    private int totalNumberOfQueues;
+    private Date averageWaitingTimeOfAllQueue;
+
+
+
 
 }
